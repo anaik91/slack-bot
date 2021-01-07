c574fa784b3c3179b4e6897619851d7a46486d26
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-07 17:56:24 +0530

    changes

--- ops-bot/app.py
+++ ops-bot/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from slack_sdk.web import WebClient
 from slackeventsapi import SlackEventAdapter
 from slack_sdk.errors import SlackApiError
-
+count=0
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
@@ -14,7 +14,7 @@ slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
 def process(channel,text):
     try:
-        response = slack_web_client.chat_postMessage(channel=channel, text=" You sent : {}".format(text))
+        response = slack_web_client.chat_postMessage(channel=channel, text="BOT_SENT : {}".format(text))
         assert response["message"]["text"] == " You sent : {}".format(text)
     except SlackApiError as e:
         # You will get a SlackApiError if "ok" is False
@@ -28,13 +28,16 @@ def message(payload):
     that contains "start".
     """
     event = payload.get("event", {})
-
     channel_id = event.get("channel")
     user_id = event.get("user")
     text = event.get("text")
+    logging.info('event ====>  {}'.format(event))
+    logging.info('channel_id ====>  {}'.format(channel_id))
+    logging.info('user_id ====>  {}'.format(user_id))
     logging.info('text ====>  {}'.format(text))
-    process(channel_id,text + 'User: ' + user_id)
-
+    if 'BOT_SENT' not in text:
+        process(channel_id,text + 'User: ' + user_id)
+    
 
 if __name__ == "__main__":
     logger = logging.getLogger()
