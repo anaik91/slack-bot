eb73cef3a9362cd3562280d0b2e590dc9f2a39e4
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-07 23:21:05 +0530

    code changes

--- ops-bot/all_blocks.py
+++ ops-bot/all_blocks.py
@@ -1,30 +1,32 @@
-help_block = [
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Dear User :wave:"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "You seem to have stumbled upon our HELP doc\n"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Below are the list of avialable commands\n• help \n • version \n • run"
-			}
-		},
-		{
-			"type": "section",
-			"text": {
-				"type": "mrkdwn",
-				"text": "Great to see you here Human ! Bot helps with you day to today needs"
-			}
-		}
-	]
+def get_help(user):
+    help_block = [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Dear <@{}> :wave:".format(user)
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "You seem to have stumbled upon our HELP doc\n"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Below are the list of avialable commands\n• help \n • version \n • run"
+                }
+            },
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "Great to see you here Human ! Bot helps with you day to today needs"
+                }
+            }
+        ]
+    return help_block
--- ops-bot/app.py
+++ ops-bot/app.py
@@ -4,7 +4,8 @@ from flask import Flask
 from slack_sdk.web import WebClient
 from slackeventsapi import SlackEventAdapter
 from slack_sdk.errors import SlackApiError
-from all_blocks import help_block
+from all_blocks import get_help
+
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
@@ -23,6 +24,7 @@ def process(channel,text=None,blocks=None):
 @slack_events_adapter.on("reaction_added")
 def update_emoji(payload):
     event = payload.get("event", {})
+    user = event.get("user")
     logging.info(event)
     channel_id = event['item']['channel']
     command_text = 'I like the Emoji. Kudos !!'
@@ -31,22 +33,23 @@ def update_emoji(payload):
 @slack_events_adapter.on("app_mention")
 def app_mention(payload):
     event = payload.get("event", {})
+    user = event.get("user")
     channel_id = event.get("channel")
     blocks = event.get("blocks")
     try:
         rich_text_elements = [ each_block['elements'][0]['elements'] for each_block in blocks if each_block['type'] == 'rich_text' ][0]
         command_data=rich_text_elements[-1]
         if command_data['type'] == 'user':
-            process(channel_id,blocks=help_block)
+            process(channel_id,blocks=get_help(user))
             return True
         else:
             command_text=command_data['text'].strip()
     except (KeyError,IndexError):
-        process(channel_id,blocks=help_block)
+        process(channel_id,blocks=get_help(user))
         return True
     logging.info(event)
     if command_text == 'help':
-        process(channel_id,blocks=help_block)
+        process(channel_id,blocks=get_help(user))
     elif command_text == 'version':
         process(channel_id,text='Version : 1.0.0')
     elif command_text == 'run':
