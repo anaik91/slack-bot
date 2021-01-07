7058dbd0518864936176dde7481dc4d7392107eb
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-07 23:05:07 +0530

    formatted help

---
+++ ops-bot/all_blocks.py
@@ -0,0 +1,30 @@
+help_block = [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Dear User :wave:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "You seem to have stumbled upon our HELP doc\n"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Below are the list of avialable commands\n• help \n • version \n • run"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "Great to see you here Human ! Bot helps with you day to today needs"
+			}
+		}
+	]
--- ops-bot/app.py
+++ ops-bot/app.py
@@ -4,7 +4,7 @@ from flask import Flask
 from slack_sdk.web import WebClient
 from slackeventsapi import SlackEventAdapter
 from slack_sdk.errors import SlackApiError
-count=0
+from all_blocks import help_block
 # Initialize a Flask app to host the events adapter
 app = Flask(__name__)
 slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/slack/events", app)
@@ -12,21 +12,9 @@ slack_events_adapter = SlackEventAdapter(os.environ["SLACK_SIGNING_SECRET"], "/s
 # Initialize a Web API client
 slack_web_client = WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 
-def get_help_command(comment):
-    help_text="""
-    {}
-
-    Avialable commands:
-    -> help
-    -> run
-    -> version
-    """.format(comment)
-
-    return help_text
-
-def process(channel,text):
+def process(channel,text=None,blocks=None):
     try:
-        response = slack_web_client.chat_postMessage(channel=channel, text=text)
+        response = slack_web_client.chat_postMessage(channel=channel, text=text,blocks=blocks)
     except SlackApiError as e:
         assert e.response["ok"] is False
         assert e.response["error"] 
@@ -38,34 +26,33 @@ def update_emoji(payload):
     logging.info(event)
     channel_id = event['item']['channel']
     command_text = 'I like the Emoji. Kudos !!'
-    process(channel_id,command_text)
+    process(channel_id,text=command_text)
 
 @slack_events_adapter.on("app_mention")
 def app_mention(payload):
     event = payload.get("event", {})
     channel_id = event.get("channel")
     blocks = event.get("blocks")
-    command_text=get_help_command('Invalid Command')
     try:
         rich_text_elements = [ each_block['elements'][0]['elements'] for each_block in blocks if each_block['type'] == 'rich_text' ][0]
         command_data=rich_text_elements[-1]
         if command_data['type'] == 'user':
-            process(channel_id,command_text)
+            process(channel_id,blocks=help_block)
             return True
         else:
             command_text=command_data['text'].strip()
     except (KeyError,IndexError):
-        process(channel_id,command_text)
+        process(channel_id,blocks=help_block)
         return True
     logging.info(event)
     if command_text == 'help':
-        process(channel_id,get_help_command('As Requested'))
+        process(channel_id,blocks=help_block)
     elif command_text == 'version':
-        process(channel_id,'Version : 1.0.0')
+        process(channel_id,text='Version : 1.0.0')
     elif command_text == 'run':
-        process(channel_id,'I ran')
+        process(channel_id,text='I ran')
     else:
-        process(channel_id,command_text)
+        process(channel_id,text=command_text)
 
 if __name__ == "__main__":
     logger = logging.getLogger()
