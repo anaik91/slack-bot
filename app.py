9727ad9a1b3e4d2d35723809a5f90c31cea78bb9
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-12 21:10:46 +0530

    added modal open

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def foo(say):
     say("I Like It !!")
 
 @app.event("app_mention")
-def event_test(body,say, ack,logger):
+def event_test(body,say, ack,logger,client):
     ack('ack')
     logger.info(body)
     event = body.get("event", {})
@@ -33,6 +33,90 @@ def event_test(body,say, ack,logger):
     logging.info('Command: {}'.format(command_text))
     m=messageHandler(command_text,user,channel_id)
     say(blocks=m.getBlock())
+
+@app.shortcut("run_command_callback")
+def open_modal(ack, shortcut, client):
+    ack()
+    client.views_open(
+        trigger_id=shortcut["trigger_id"],
+        view={
+            "type": "modal",
+            "submit": {
+                "type": "plain_text",
+                "text": "Submit",
+                "emoji": True
+            },
+            "close": {
+                "type": "plain_text",
+                "text": "Cancel",
+                "emoji": True
+            },
+            "title": {
+                "type": "plain_text",
+                "text": "Run Command Menu",
+                "emoji": True
+            },
+            "blocks": [
+                {
+                    "type": "divider"
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "static_select",
+                        "placeholder": {
+                            "type": "plain_text",
+                            "text": "Select an item",
+                            "emoji": True
+                        },
+                        "options": [
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "EU10#192.168.1.1",
+                                    "emoji": True
+                                },
+                                "value": "value-0"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "EU10#192.168.1.2",
+                                    "emoji": True
+                                },
+                                "value": "value-1"
+                            },
+                            {
+                                "text": {
+                                    "type": "plain_text",
+                                    "text": "EU10#192.168.1.3",
+                                    "emoji": True
+                                },
+                                "value": "value-2"
+                            }
+                        ],
+                        "action_id": "node_ip"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Choose Server",
+                        "emoji": True
+                    }
+                },
+                {
+                    "type": "input",
+                    "element": {
+                        "type": "plain_text_input",
+                        "action_id": "node_command"
+                    },
+                    "label": {
+                        "type": "plain_text",
+                        "text": "Command to Run",
+                        "emoji": True
+                    }
+                }
+            ]
+        })
 
 flask_app = Flask(__name__)
 handler = SlackRequestHandler(app)
