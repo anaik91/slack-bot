bad9e2435f075b9348963e7f98568ff95a0e89da
Ashwin Kumar Naik <ashwin.kumar.naik@sap.com>
2021-01-12 17:34:02 +0530

    health check

--- bolt_app.py
+++ bolt_app.py
@@ -15,7 +15,7 @@ def event_test(body, say, ack,logger):
     logger.info(body)
     say("Hi from Google Cloud Functions!")
 
-from flask import Flask, request
+from flask import Flask, request ,jsonify
 from slack_bolt.adapter.flask import SlackRequestHandler
 
 flask_app = Flask(__name__)
@@ -24,6 +24,10 @@ handler = SlackRequestHandler(app)
 @flask_app.route("/slack/events", methods=["POST"])
 def slack_events():
     return handler.handle(request)
+
+@flask_app.route('/ping')
+def ping():
+    return jsonify({'status': 'ok'})
 
 if __name__ == '__main__':
     flask_app.run(host='0.0.0.0',port=8080)
